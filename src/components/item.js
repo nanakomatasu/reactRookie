a1b453923262f8b277ef783ce4fbbdd08b4db960
nanakomatasu <123913335+nanakomatasu@users.noreply.github.com>
2023-02-05 09:40:47 +0800

    添加新元件

---
+++ src/components/item.js
@@ -0,0 +1,12 @@
+const Item = () => {
+  return (
+    <ul>
+      <li>1</li>
+      <li>2</li>
+      <li>3</li>
+      <li>4</li>
+    </ul>
+  );
+};
+
+export default Item;
---
+++ src/components/xiang.js
@@ -0,0 +1,9 @@
+import List from "./list.js";
+const XI = () => {
+  return (
+    <div>
+      <List />
+    </div>
+  );
+};
+export default XI;
